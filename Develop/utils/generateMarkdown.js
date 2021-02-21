5801d961b5b40c1eb647cdbceec3258ab6945204
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-02-21 16:27:52 -0500

    Added table of contents

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,39 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+## Table of Contents
+*[Description] (#description)
+*[Installation] (#installation)
+*[Usage] (#usage)
+*[License] (#license)
+*[Contributors] (#contributors)
+*[Test] (#test)
+*[Questions] (#questions)
 
+##Description
+${data.Description}
+
+##Installation
+${data.Installation}
+
+##Usage
+${data.Usage}
+
+##License
+${data.License}
+
+##Contributors
+${data.Contributors}
+
+##Test
+${data.Test}
+
+##Questions
+Contact Info:
+
+Github: [${data.Username}] (https://github.com/krrodriguez26)
+Email: [${data.Email}] (https://github.com/krrodriguez26)
 `;
 }
 
